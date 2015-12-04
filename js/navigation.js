8794d3f78e6db2c91f494730d7a841dff9fb1892
Dominic Hickey <dominic.j.hickey@gmail.com>
2015-12-04 10:33:46 +0000

    Added files from Facet template

---
+++ js/navigation.js
@@ -0,0 +1,30 @@
+// Toggles class open to display/hide the whole navigation
+
+$( ".custom-navigation-toggle" ).click(function() {
+	$( ".bk-extendednavigation, .drop-down-nav" ).toggleClass( "open" );
+});
+
+// Togglse class open to display/hide folders
+
+$( ".navigation-item.folder .item-name--parent" ).each(function() {
+	$( this ).click(function() {
+			$( this ).parent( ".navigation-item.folder" ).toggleClass( "open" );
+	});
+});
+
+// Hides custom toggle button when navigation is hidden
+
+var hiddenNavHandler = function () {
+    if (
+        ($("#page-zones__template-widgets__extendednavigation-extendednavigation").length == 0)
+        ||
+        ($("#page-zones__template-widgets__extendednavigation-extendednavigation.bk-tpl-hidden").length == 1)
+    )
+    {
+       $(".drop-down-nav").addClass("nav-hidden");
+    }
+};
+
+hiddenNavHandler();
+$(document).ready(hiddenNavHandler);
+$(window).resize(hiddenNavHandler);
